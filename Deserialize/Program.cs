4068d9ba5f8b22b6d98bc2e3b207b176ccc4b248
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-12-03 10:44:37 +0500

    Добавьте файлы проекта.

---
+++ Deserialize/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Deserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            string path = "C:\\Автоматизация bim проектирования\\С#\\Домашка 16\\Lab16\\Serialize\\bin\\Debug\\Lab\\Product.json";
+            StreamReader streamReader = new StreamReader(path, false);
+
+            line = streamReader.ReadToEnd();
+            streamReader.Close();
+            Product[] products2 = JsonSerializer.Deserialize<Product[]>(line);
+
+            double max = products2[0].Price;
+            int maxIndex = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (products2[i].Price > max)
+                {
+                    max = products2[i].Price;
+                    maxIndex = i;
+                }
+            }
+            Console.WriteLine("Товар с максимальной ценой:");
+            Console.WriteLine("Код:" + products2[maxIndex].Code);
+            Console.WriteLine("Наименование:" + products2[maxIndex].Name);
+            Console.WriteLine("Цена:" + products2[maxIndex].Price + "руб.");
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
---
+++ Serialize/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+
+namespace Serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Lab/Product.json";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+
+            }
+            Console.WriteLine("Введите параметры пяти товаров в указанной форме:");
+            Console.WriteLine("код");
+            Console.WriteLine("наименование");
+            Console.WriteLine("цена, руб.");
+            Product[] products = new Product[]
+            {
+                new Product
+                {
+
+                    Code=Convert.ToInt32(Console.ReadLine()),
+                    Name=Convert.ToString(Console.ReadLine()),
+                    Price=Convert.ToDouble(Console.ReadLine())
+                },
+                new Product
+                {
+                    Code=Convert.ToInt32(Console.ReadLine()),
+                    Name=Convert.ToString(Console.ReadLine()),
+                    Price=Convert.ToDouble(Console.ReadLine())
+                },
+                new Product
+                {
+                    Code=Convert.ToInt32(Console.ReadLine()),
+                    Name=Convert.ToString(Console.ReadLine()),
+                    Price=Convert.ToDouble(Console.ReadLine())
+                },
+                new Product
+                {
+                    Code=Convert.ToInt32(Console.ReadLine()),
+                    Name=Convert.ToString(Console.ReadLine()),
+                    Price=Convert.ToDouble(Console.ReadLine())
+                },
+                new Product
+                {
+                    Code=Convert.ToInt32(Console.ReadLine()),
+                    Name=Convert.ToString(Console.ReadLine()),
+                    Price=Convert.ToDouble(Console.ReadLine())
+                },
+
+            };
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsstring = JsonSerializer.Serialize(products, options);
+            using (StreamWriter stream = new StreamWriter(path, false))
+            {
+                stream.WriteLine(jsstring);
+            }
+
+            Console.WriteLine(jsstring);
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
